ac61deecba7f369107b1271f96ec5f5d1a6ce55b
x-CHIEF-x <155864552+x-CHIEF-x@users.noreply.github.com>
2024-01-26 06:07:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Developed by CHIEF"];
 
 
 let currentIndex = 0;
